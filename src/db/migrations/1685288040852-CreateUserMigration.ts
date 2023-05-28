4c49bd1a336a9fbddf6e5af96358c35c13caf24f
Mettle X <56449790+mettlex@users.noreply.github.com>
2023-05-29 02:43:43 +0600

    chore: format code

--- src/db/migrations/1685288040852-CreateUserMigration.ts
+++ src/db/migrations/1685288040852-CreateUserMigration.ts
@@ -1,14 +1,15 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class CreateUserMigration1685288040852 implements MigrationInterface {
-    name = 'CreateUserMigration1685288040852'
+  name = "CreateUserMigration1685288040852";
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "users" ("discordId" varchar PRIMARY KEY NOT NULL, "isEligible" boolean NOT NULL DEFAULT (0), "walletBalance" double NOT NULL DEFAULT (0))`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE "users" ("discordId" varchar PRIMARY KEY NOT NULL, "isEligible" boolean NOT NULL DEFAULT (0), "walletBalance" double NOT NULL DEFAULT (0))`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`DROP TABLE "users"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE "users"`);
+  }
 }
--- src/db/migrations/1685300212355-AddCreatedAtAndUpdatedAtToUserMigration.ts
+++ src/db/migrations/1685300212355-AddCreatedAtAndUpdatedAtToUserMigration.ts
@@ -1,20 +1,29 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class AddCreatedAtAndUpdatedAtToUserMigration1685300212355 implements MigrationInterface {
-    name = 'AddCreatedAtAndUpdatedAtToUserMigration1685300212355'
+export class AddCreatedAtAndUpdatedAtToUserMigration1685300212355
+  implements MigrationInterface
+{
+  name = "AddCreatedAtAndUpdatedAtToUserMigration1685300212355";
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "temporary_users" ("discordId" varchar PRIMARY KEY NOT NULL, "isEligible" boolean NOT NULL DEFAULT (0), "walletBalance" double NOT NULL DEFAULT (0), "createdAt" datetime DEFAULT (datetime('now')), "updatedAt" datetime DEFAULT (datetime('now')))`);
-        await queryRunner.query(`INSERT INTO "temporary_users"("discordId", "isEligible", "walletBalance") SELECT "discordId", "isEligible", "walletBalance" FROM "users"`);
-        await queryRunner.query(`DROP TABLE "users"`);
-        await queryRunner.query(`ALTER TABLE "temporary_users" RENAME TO "users"`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE "temporary_users" ("discordId" varchar PRIMARY KEY NOT NULL, "isEligible" boolean NOT NULL DEFAULT (0), "walletBalance" double NOT NULL DEFAULT (0), "createdAt" datetime DEFAULT (datetime('now')), "updatedAt" datetime DEFAULT (datetime('now')))`,
+    );
+    await queryRunner.query(
+      `INSERT INTO "temporary_users"("discordId", "isEligible", "walletBalance") SELECT "discordId", "isEligible", "walletBalance" FROM "users"`,
+    );
+    await queryRunner.query(`DROP TABLE "users"`);
+    await queryRunner.query(`ALTER TABLE "temporary_users" RENAME TO "users"`);
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "users" RENAME TO "temporary_users"`);
-        await queryRunner.query(`CREATE TABLE "users" ("discordId" varchar PRIMARY KEY NOT NULL, "isEligible" boolean NOT NULL DEFAULT (0), "walletBalance" double NOT NULL DEFAULT (0))`);
-        await queryRunner.query(`INSERT INTO "users"("discordId", "isEligible", "walletBalance") SELECT "discordId", "isEligible", "walletBalance" FROM "temporary_users"`);
-        await queryRunner.query(`DROP TABLE "temporary_users"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "users" RENAME TO "temporary_users"`);
+    await queryRunner.query(
+      `CREATE TABLE "users" ("discordId" varchar PRIMARY KEY NOT NULL, "isEligible" boolean NOT NULL DEFAULT (0), "walletBalance" double NOT NULL DEFAULT (0))`,
+    );
+    await queryRunner.query(
+      `INSERT INTO "users"("discordId", "isEligible", "walletBalance") SELECT "discordId", "isEligible", "walletBalance" FROM "temporary_users"`,
+    );
+    await queryRunner.query(`DROP TABLE "temporary_users"`);
+  }
 }
